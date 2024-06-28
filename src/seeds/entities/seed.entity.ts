a0f31a6b81f92c933907c27ae565a0a9f416d3e4
Omprakash <omprakashuppar123@gmail.com>
2024-06-28 11:59:15 +0530

    Modified Files to relative path

--- src/seeds/entities/seed.entity.ts
+++ src/seeds/entities/seed.entity.ts
@@ -4,7 +4,8 @@ import { Product } from "src/product/entities/product.entity";
 import { UserDetail } from "src/user/entities/user-detail.entity";
 import { UserLogs } from "src/user/entities/user-log.entity";
 import { User } from "src/user/entities/user.entity";
-import { ProfileLog } from "src/userprofile/entities/profilelog.entity";
+// import { ProfileLog } from "src/userprofile/entities/profilelog.entity";
+ import { ProfileLog } from "../../userprofile/entities/profilelog.entity";
 import { UserProfile } from "src/userprofile/entities/userprofile.entity";
 import { Users } from "src/userprofile/entities/users.entity";
 
